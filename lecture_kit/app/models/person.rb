5718f843831acff84c4dccb67301d2a08df11c20
Joanna <astronautnemo@gmail.com>
2023-02-24 17:29:21 +0300

    completed third deliverable

--- lecture_kit/app/models/person.rb
+++ lecture_kit/app/models/person.rb
@@ -5,4 +5,17 @@ class Person < ActiveRecord::Base
 
     has_many :waterings
     has_many :waterees, through: :waterings
+
+    def water_plant(plant)
+        Watering.create(person_id: self, plant_id: plant.id)
+        pp = plant_parenthoods.find_by(plant: plant) 
+        binding.pry
+
+        if pp
+            affection = pp.affection || 1
+            affection += 1
+            pp.update(affection: affection)
+        end
+        
+    end
 end
